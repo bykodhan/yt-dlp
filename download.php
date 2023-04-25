62bcf54c7a1364257853e9114788132de8102db0
Mustafa Eren Demir <bykodhan@gmail.com>
2023-04-25 22:24:15 +0300

    Update download.php

--- download.php
+++ download.php
@@ -5,28 +5,4 @@ error_reporting(E_ALL);
 $url = $_GET['url'];
 $result = shell_exec("yt-dlp " . $url);
 var_dump($result);
-telegram_bot_send_message($result);
-function telegram_bot_send_message($message)
-{
-    $token = "";
-    $parametre = array(
-        'chat_id' => "",
-        'video' => $message,
-    );
-    $ch = curl_init();
-    $url = "https://api.telegram.org/bot" . $token . "/sendVideo";
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $parametre);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    if (json_decode($result)->ok) {
-        return true;
-    } else {
-
-    }
-}
-
 ?>
