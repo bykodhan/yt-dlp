0cb25a0750ea5974aff5d2b4c5a9ba0ef098ec82
Mustafa Eren Demir <bykodhan@gmail.com>
2023-04-25 22:25:03 +0300

    Update download.php

--- download.php
+++ download.php
@@ -3,6 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $url = $_GET['url'];
-$result = shell_exec("yt-dlp " . $url);
+$result = shell_exec("yt-dlp --get-url " . $url);
 var_dump($result);
 ?>
